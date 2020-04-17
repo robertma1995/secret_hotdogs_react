ccd6a93beb44f328b1e77b2445ce21fb12772856
robertma1995 <r.ma@student.unsw.edu.au>
2020-04-17 12:57:08 +1000

    added simple import exports in server code

--- server/routes.js
+++ server/routes.js
@@ -1,11 +1,14 @@
-import * as express from 'express';
+// import * as express from 'express';
+const express = require('express');
+
 
 const router = express.Router();
 
+// TODO: one route for each table (hotdogs, users)
 // testing api
 router.get('/api/hello', (req, res, next) => {
-	res.json('Hello World :)');
+	res.json('Hello World penis :)');
 });
 
-export default apiRouter;
-// TODO: one route for each table (hotdogs, users)
+// export default apiRouter;
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -31,16 +31,19 @@
 
 // app.listen(process.env.PORT || 3000);
 
-
+// imports
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 5000;
+const apiRouter = require('./routes.js');
 
+const app = express();
+app.use(apiRouter);
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-// TODO: figure out a way to "import" router.js
-// app.use(express.static(path.join(__dirname, 'build')));
-// TEST: simple api
-app.get('/api/hello', (req, res, next) => {
-    res.json('Hello World smile smile :)');
-});
+// // TODO: figure out a way to "import" router.js
+// // app.use(express.static(path.join(__dirname, 'build')));
+// // TEST: simple api
+// app.get('/api/hello', (req, res, next) => {
+//     res.json('Hello World smile smile :)');
+// });
