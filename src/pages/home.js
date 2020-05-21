6ae0260c65110ab8e884886204ed1b7758a7a98b
robertma1995 <r.ma@student.unsw.edu.au>
2020-05-21 14:49:42 +1000

    redirects from home and login depending on logged in status

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Box, Container } from '@material-ui/core';
 import { HomeHotdogGrid, PageTitle } from '../components'; 
+// routing
+import { Redirect } from 'react-router-dom';
+import * as routes from '../utils/routes';
+// context
+import { UserContext } from '../userContext';
 
-function Home() {    
+function Home() {
+    // redirect to login page if user not logged in
+    const { userId } = useContext(UserContext);
+    if (!userId) {
+        return (
+            <Redirect to={routes.LOGIN}/>
+        );
+    }
+
     return (
         <Container>
             <Box p={2}>
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Box, Container } from '@material-ui/core';
 import { LoginForm, PageTitle } from '../components';
+// routing
+import { Redirect } from 'react-router-dom';
+import * as routes from '../utils/routes';
+// context
+import { UserContext } from '../userContext';
 
 function Login() {
+	// redirect to home page if user already logged in
+    const { userId } = useContext(UserContext);
+    if (userId) {
+        return (
+            <Redirect to={routes.HOME}/>
+        );
+    }
+
     return (
         <Container maxWidth="xs">
             <Box p={2}>
