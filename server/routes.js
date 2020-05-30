d21785eaa5ca3d25436d3cd54503d11c1448524f
robertma1995 <r.ma@student.unsw.edu.au>
2020-05-30 13:00:12 +1000

    changed register api endpoint

--- server/routes.js
+++ server/routes.js
@@ -5,12 +5,16 @@
 const express = require('express');
 const DB = require('./database');
 
+
+// initialize router
 const router = express.Router();
+
 // testing api
 router.get('/api/hello', (req, res, next) => {
     res.json('Hello World :)');
 });
 
+// ========================================= HOTDOGS =========================================
 // gets all hotdogs of every user
 router.get('/api/hotdogs/all', async (req, res) => {
     try {
@@ -34,7 +38,8 @@ router.get('/api/hotdogs/createdBy/:id', async (req, res) => {
     }
 });
 
-// TODO: add a new hotdog
+// add a new hotdog
+// TODO: remove "add" endpoint
 router.post('/api/hotdogs/add', async (req, res) => {
     // const { creatorId, title, sausage, sauce, toppingA, toppingB } = req.body;
     try {
@@ -55,12 +60,26 @@ router.get('/api/hotdogs/:id', async (req, res) => {
 });
 */
 
+// ========================================= USERS =========================================
 // gets details of user from "users" collection (not firebase.auth)
 router.get('/api/users/:id', async (req, res) => {
     const id = req.params.id;
     try {
         let user = await DB.users.get(id);
         res.json(user);
+    } catch(e) {
+        console.log(e);
+        res.json(false);
+    }
+});
+
+// registration: logs user id for backend debugging
+router.post('/api/users', async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        let userId = await DB.users.register(name, email, password);
+        console.log("User with id " + userId + " successfully created");
+        res.json(true);
     } catch(e) {
         console.log(e);
         res.json(false);
@@ -79,17 +98,5 @@ router.post('/api/login', async (req, res) => {
     }
 });
 
-// registration: returns true if successful, logs user id for backend debugging
-router.post('/api/register', async (req, res) => {
-    const { name, email, password } = req.body;
-    try {
-        let userId = await DB.users.register(name, email, password);
-        console.log("User with id " + userId + " successfully created");
-        res.json(true);
-    } catch(e) {
-        console.log(e);
-        res.json(false);
-    }
-});
 
 module.exports = router;
