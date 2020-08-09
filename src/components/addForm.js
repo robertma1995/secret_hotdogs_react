2fc61401bb705a1fefa0721c8f98cc91b5da4240
robertma1995 <r.ma@student.unsw.edu.au>
2020-08-09 11:15:37 +1000

    added plan for dynamic form fields

--- src/components/addForm.js
+++ src/components/addForm.js
@@ -30,6 +30,143 @@ function isValid(input, setInput, setInputError) {
     return inputValid;
 }
 
+function AddFormTest() {
+    const [title, setTitle] = useState("");
+    const [titleError, setTitleError] = useState(" ");
+    const [sausage, setSausage] = useState("");
+    const [sausageError, setSausageError] = useState(" ");
+    const [sauce, setSauce] = useState("");
+    const [sauceError, setSauceError] = useState(" ");
+    // const [toppingA, setToppingA] = useState("");
+    // const [toppingAError, setToppingAError] = useState(" ");
+    // const [toppingB, setToppingB] = useState("");
+    // const [toppingBError, setToppingBError] = useState(" ");
+    const [toppings, setToppings] = useState([]);
+    /*
+    for each topping added, declare new state variables 
+
+        const [topping0, setTopping0] = useState("")
+        const [topping0Error, setTopping0Error] = useState(" ")
+
+    and push a new entry to toppings:
+
+        "0": {
+            label: "Topping 0"
+            value: topping0
+            setValue: setTopping0
+            error: topping0Error
+        }
+    */
+    
+    const { userId, userName } = useContext(UserContext);
+    const [loading, setLoading] = useState(false);
+    const [added, setAdded] = useState(false);
+
+    function handleAdd() {
+        // set added to false again to handle consecutive adds on same page (no reload)
+        setAdded(false);
+        // var titleValid, sausageValid, sauceValid, toppingAValid, toppingBValid;
+         var titleValid, sausageValid, sauceValid, toppingsValid;
+        titleValid = sausageValid = sauceValid = toppingsValid = false;
+
+        titleValid = isValid(title, setTitle, setTitleError);
+        sausageValid = isValid(sausage, setSausage, setSausageError);
+        sauceValid = isValid(sauce, setSauce, setSauceError);
+        // toppingAValid = isValid(toppingA, setToppingA, setToppingAError);
+        // toppingBValid = isValid(toppingB, setToppingB, setToppingBError);
+        // TODO: check isValid for all toppings
+        toppingsValid = false;
+
+        // if (titleValid && sausageValid && sauceValid && toppingAValid && toppingBValid) {
+        if (titleValid && sausageValid && sauceValid && toppingsValid) {
+            setLoading(true);
+            (async () => {
+                // trim again just in case, since set<value>(<value>Trimmed) is asynchronous
+                // TODO: after state variable stuff figured out, change backend stuff
+                const hotdog = {
+                    creatorId: userId,
+                    creatorName: userName,
+                    title: title.trim(),
+                    ingredients: {
+                        sausage: sausage.trim(),
+                        sauce: sauce.trim(),
+                        // toppingA: toppingA.trim(),
+                        // toppingB: toppingB.trim(),
+                    }
+                }
+                const addStatus = await DB.addHotdog(hotdog);
+                setLoading(false);
+
+                // if add hotdog succeeds, reset all fields and give user option to go back to homepage 
+                // no restrictions on field values for now, so shouldn't fail
+                if (!addStatus) {
+                    console.log("addForm.js: something went wrong :(");
+                } else {
+                    setTitle("");
+                    setSausage("");
+                    setSauce("");
+                    // setToppingA("");
+                    // setToppingB("");
+                    setAdded(true);
+                }
+            })();
+        }
+    }
+
+    return (
+        <Form>
+            <FormField
+                type="text"
+                iconName="hotdogTitle"
+                label="Title"
+                value={title}
+                setValue={setTitle}
+                error={titleError}
+            />
+            <FormField
+                type="text"
+                iconName="hotdogSausage"
+                label="Sausage"
+                value={sausage}
+                setValue={setSausage}
+                error={sausageError}
+            />
+            <FormField
+                type="text"
+                iconName="hotdogSauce"
+                label="Sauce"
+                value={sauce}
+                setValue={setSauce}
+                error={sauceError}
+            />
+            { toppings.map(topping => (
+                <FormField
+                    type="text"
+                    iconName="hotdogTopping"
+                    // label="Topping A"
+                    label={topping.label}
+                    // value={toppingA}
+                    value={topping.value}
+                    // setValue={setToppingA}
+                    setValue={topping.setValue}
+                    // error={toppingAError}
+                    error={topping.error}
+                />
+            ))}
+            <FormButton
+                text="Submit"
+                loading={loading}
+                handleClick={handleAdd}
+            />
+            <SuccessSnackbar
+                open={added}
+                setOpen={setAdded}
+                message="Posted new hotdog!"
+            />
+        </Form>
+    );
+} 
+
 function AddForm() {
     const [title, setTitle] = useState("");
     const [titleError, setTitleError] = useState(" ");
